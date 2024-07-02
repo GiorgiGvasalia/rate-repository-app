fee9360474d7ebd140c8ca14df9d3643f57cb2d6
GiorgiGvasalia <ggvasalia4@gmail.com>
2024-07-02 15:03:32 +0400

    rounded up counts for review

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -22,6 +22,15 @@ const styles = StyleSheet.create({
 });
 
 const RepositoryItem = ({ repository }) => {
+  // if count > 999 it will be displayed in 'k'(thousand)
+  const checkCounts = (count) => {
+    if (count >= 1000) {
+      const roundedCount = (count / 1000).toFixed(1); 
+      return roundedCount + "k";
+    } else {
+      return count.toString(); 
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.flexItems}>
@@ -36,9 +45,9 @@ const RepositoryItem = ({ repository }) => {
         </View>
       </View>
       <View style={styles.flexItems}>
-        <Text>Stars: {repository.stargazersCount}</Text>
-        <Text>Forks: {repository.forksCount}</Text>
-        <Text>Reviews: {repository.reviewCount}</Text>
+        <Text>Stars: {checkCounts(repository.stargazersCount)}</Text>
+        <Text>Forks: {checkCounts(repository.forksCount)}</Text>
+        <Text>Reviews: {checkCounts(repository.reviewCount)}</Text>
         <Text>Rating: {repository.ratingAverage}</Text>
       </View>
     </View>
